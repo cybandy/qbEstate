3d62a628cd1689cfcfc6954ef65d39576546ba52
Cybandy <andyboat75@gmail.com>
2024-04-15 02:42:01 +0200

    pulled supabase schema, installed other nuxt modules

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    "@pinia/nuxt",
+    "@nuxt/image",
+    '@pinia-plugin-persistedstate/nuxt',
+    "nuxt-lodash",
+    "@nuxt/ui",
+    '@nuxtjs/supabase',
+    "@nuxt/fonts",
+    "@nuxt/eslint"
+  ],
+  lodash: {
+    prefix: "_",
+    prefixSkip: false
+  },
+  fonts: {
+    families: [
+      { name: 'Nunito Sans', provider: 'google', weights: ['400', '500', '600', '700'] }
+    ]
+  }
 })
